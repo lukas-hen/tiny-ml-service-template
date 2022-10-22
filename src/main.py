7b3e74c29ec744dd6b139fe95450eca4cfb21db6
lukas-hen <hennicks.lukas@gmail.com>
2022-10-22 22:05:26 +0200

    Refactored out model predict check to separate function

--- src/main.py
+++ src/main.py
@@ -14,7 +14,31 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
+    assert_all_models_implement_predict(models_path)
 
+
+@app.post("/api/infer")
+async def predict_latest_model(data: InferenceInput):
+    """
+    Run inference using the latest model SemVer.
+
+    Api endpoint to run inference, but automatically choose the latest model.
+    """
+    model_version = get_latest_model(models_path)
+    return infer(data, model_version)
+
+
+@app.post("/api/infer/{model_version}")
+async def predict_specific_model(data: InferenceInput, model_version: str):
+    """
+    Run inference using specific model version.
+
+    Api endpoint to run inference using the provided model version in the url path.
+    """
+    return infer(data, model_version)
+
+
+def assert_all_models_implement_predict(models_path):
     """
     Initial check that all given models implement the predict method.
     """
@@ -38,24 +62,3 @@ async def startup():
     """ Close models """
     for model_file in model_files:
         model_file.close()
-
-
-@app.post("/api/infer")
-async def predict_latest_model(data: InferenceInput):
-    """
-    Run inference using the latest model SemVer.
-
-    Api endpoint to run inference, but automatically choose the latest model.
-    """
-    model_version = get_latest_model(models_path)
-    return infer(data, model_version)
-
-
-@app.post("/api/infer/{model_version}")
-async def predict_specific_model(data: InferenceInput, model_version: str):
-    """
-    Run inference using specific model version.
-
-    Api endpoint to run inference using the provided model version in the url path.
-    """
-    return infer(data, model_version)
